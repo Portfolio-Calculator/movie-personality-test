346946ee0345ee6f9d6e038bcc18acec2385f17d
Divya <dm.divya.mahajan@gmail.com>
2023-05-30 10:16:05 -0700

    updated Popularity

--- src/app/results/page.js
+++ src/app/results/page.js
@@ -7,16 +7,59 @@ export default function Results(props) {
   // console.log("props: ", Object.keys(props.searchParams)[0])
 
   const searchQuery = Object.keys(props.searchParams)[0];
+  const movies = moviesData.map((movie) => ({
+    // Preprocess the movie data
+    ...movie,
+    genres: movie.genres,
+    tags: movie.tags.toLowerCase(),
+    storyline: movie.storyline,
+  }));
 
+  // function getMovieRecommendations(query) {
+  //   // Calculate cosine similarity
+  //   function calculateCosineSimilarity(query, movies) {
+  //     const queryVector = query.toLowerCase().split(" ");
+  //     const similarities = movies.map((movie) => {
+  //       const movieVector = movie.tags.split(" ");
+  //       const intersection = queryVector.filter((tag) =>
+  //         movieVector.includes(tag)
+  //       );
+  //       const cosineSimilarity =
+  //         intersection.length /
+  //         Math.sqrt(queryVector.length * movieVector.length);
+  //       return cosineSimilarity;
+  //     });
+  //     return similarities;
+  //   }
+
+  //   // Get movie recommendations based on a search query
+  //   function getMovieRecommendations(query, movies, topN = 5) {
+  //     // Calculate cosine similarity
+  //     const similarities = calculateCosineSimilarity(query, movies);
+
+  //     // Sort by similarity
+  //     const sortedIndices = similarities
+  //       .map((similarity, index) => ({ index, similarity }))
+  //       .sort((a, b) => b.similarity - a.similarity);
+
+  //     // Get top indices based on similarity
+  //     const topIndices = sortedIndices.slice(0, topN).map((item) => item.index);
+
+  //     // Get final recommendations with genre and keywords
+  //     const recommendations = topIndices.map((index) => ({
+  //       movieId: movies[index].movie_id,
+  //       title: movies[index].title,
+  //       genres: movies[index].genres,
+  //       keywords: movies[index].tags,
+  //       overview: movies[index].storyline,
+  //     }));
+
+  //     return recommendations;
+  //   }
+
+  //   return getMovieRecommendations(query, movies);
+  // }
   function getMovieRecommendations(query) {
-    const movies = moviesData.map((movie) => ({
-      // Preprocess the movie data
-      ...movie,
-      genres: movie.genres,
-      tags: movie.tags.toLowerCase(),
-      storyline: movie.storyline,
-    }));
-
     // Calculate cosine similarity
     function calculateCosineSimilarity(query, movies) {
       const queryVector = query.toLowerCase().split(" ");
@@ -32,34 +75,39 @@ export default function Results(props) {
       });
       return similarities;
     }
-
+  
     // Get movie recommendations based on a search query
-    function getMovieRecommendations(query, movies, topN = 5) {
+    function getMovieRecommendations(query, movies, topN = 20) {
       // Calculate cosine similarity
       const similarities = calculateCosineSimilarity(query, movies);
-
+  
       // Sort by similarity
       const sortedIndices = similarities
         .map((similarity, index) => ({ index, similarity }))
         .sort((a, b) => b.similarity - a.similarity);
-
+  
       // Get top indices based on similarity
       const topIndices = sortedIndices.slice(0, topN).map((item) => item.index);
-
-      // Get final recommendations with genre and keywords
+  
+      // Get final recommendations with genre, keywords, and popularity
       const recommendations = topIndices.map((index) => ({
         movieId: movies[index].movie_id,
         title: movies[index].title,
         genres: movies[index].genres,
         keywords: movies[index].tags,
         overview: movies[index].storyline,
+        popularity: movies[index].popularity, // Assuming a popularity score exists for movies
       }));
-
+  
+      // Sort recommendations by popularity (descending order)
+      recommendations.sort((a, b) => b.popularity - a.popularity);
+  
       return recommendations;
     }
-
+  
     return getMovieRecommendations(query, movies);
   }
+
   const movieRecommendations = getMovieRecommendations(searchQuery);
   // console.log(movieRecommendations);
   // movieRecommendations.forEach((movie) => {
